50f5c6f3ce0812d040aacfdb583c691f3d1e60f9
maoxiangmin <maoxiangmin@gyyx.cn>
2017-11-22 20:34:34 +0800

    update:调整

--- webskill-root/src/App.vue
+++ webskill-root/src/App.vue
@@ -6,17 +6,104 @@
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body,
+div,
+dl,
+dt,
+dd,
+ul,
+ol,
+li,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+pre,
+form,
+fieldset,
+input,
+textarea,
+p,
+blockquote,
+th,
+td {
+  margin: 0;
+  padding: 0;
+}
+body {
+  font: 12px "Microsoft YaHei";
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+textarea {
+  resize: none;
+}
+fieldset,abbr,
+acronym {
+  border: 0;
+}
+img {
+  vertical-align: bottom;
+  border-style: none;
+  border: 0 none;
+  outline: none;
+}
+address,
+caption,
+cite,
+code,
+dfn,
+th,
+var {
+  font-style: normal;
+  font-weight: normal;
+}
+ol,
+ul {
+  list-style: none;
+}
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-size: 100%;
+  font-weight: normal;
+}
+i {
+  font-style: normal;
+}
+.clear {
+  zoom: 1;
+}
+.clear:after {
+  content: "";
+  display: block;
+  clear: both;
+}
+a {
+  text-decoration: none;
+  cursor: pointer;
+}
+a:focus,
+input:focus,
+textarea:focus,
+select:focus {
+  outline-style: none;
 }
 </style>
--- webskill-root/src/router/index.js
+++ webskill-root/src/router/index.js
@@ -10,15 +10,13 @@ const router = new Router({
   linkActiveClass: 'active',
   hashbang: false, // 将路径格式化为#!开头
   history: true, // 启用HTML5 history模式，可以使用pushState和replaceState来管理记录
-  routes: [
-    {
-      path: '/',
-      name: 'Index',
-      components: {
-        default: Index
-      }
+  routes: [{
+    path: '/',
+    name: 'Index',
+    components: {
+      default: Index
     }
-  ]
+  }]
 })
 
 /**
