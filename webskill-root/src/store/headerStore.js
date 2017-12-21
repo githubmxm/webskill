4b8809309421dd906d1e83c308462ad1399f5628
mxm <1123360735@qq.com>
2017-12-21 23:47:32 +0800

    update:添加简要数据方法

--- webskill-root/src/store/headerStore.js
+++ webskill-root/src/store/headerStore.js
@@ -1,13 +1,23 @@
-
+import axios from 'axios'
 export default{
   state: {
     //页签
     navT:[]
   },
   mutations: {
-    
+    setNav(state,navs){
+       state.navT=navs;
+    }
   },
   actions: {
-    
+    getNav({commit},url){
+      axios({
+        method:get,
+        url:url
+      }).then((res)=>{
+         let data=res.data.data
+         commit('setNavs',data)
+      })
+    }
   }
 }
