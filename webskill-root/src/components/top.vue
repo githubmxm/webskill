57c01a00317280e4f0f2020f2a16b2c81ff4cded
maoxiangmin <maoxiangmin@gyyx.cn>
2018-01-02 23:48:38 +0800

    update:简要模板

--- webskill-root/src/components/top.vue
+++ webskill-root/src/components/top.vue
@@ -1,50 +1,68 @@
 <template>
   <ul class="top">
-    <li v-for="(k,v) in navT" :id="k">{{v}}</li>
+    <li class="mainNavList" v-for="navs in mainNavs" :id="navs.mainNavId" :navid="navs.mainNavId" @mouseenter="mainNavEnter($event)" @mouseleave="mainNavLeave($event)">
+      <a href="">{{navs.mainNavName}}</a>
+      <ul class="deputyNavs">
+        <li v-for="deputyNave in navs.deputyNav" :id="deputyNave.deputyNavId" :deputynavid="deputyNave.deputyNavId">
+          <a href="">{{deputyNave.deputyNavName}}</a>
+        </li>
+      </ul>
+    </li>
   </ul>
 </template>
 
 <script>
-import {mapState,mapMutations,mapActions} from 'vuex'
+import { mapState, mapMutations, mapActions } from "vuex";
 export default {
   name: "top",
   data() {
     return {
-      msg: "公共头部导航"
+      msg: "主导航和二级导航展示"
     };
   },
-  created () {
-    
-  },
+  created() {},
   computed: {
     ...mapState({
-      navT:state=>state.HeaderStore.navT
+      mainNavs: state => state.HeaderStore.mainNavs
     })
   },
   methods: {
-    ...mapActions([
-      'getNav'
-    ])
+    ...mapActions(["getNav"]),
+    mainNavEnter:function(ele){
+      if(ele.target.children[1]){
+        console.log( ele.target.children[1].style)
+         ele.target.children[1].style.display="block";
+      }
+    },
+    mainNavLeave:function(ele){
+      if(ele.target.children[1]){
+        console.log( ele.target.children[1].style)
+         ele.target.children[1].style.display="none";
+      }       
+    }
   },
-  mounted:function(){
+  mounted: function() {
     this.getNav("/static/headJson.json");
   }
 };
 </script>
 
 <style lang="scss" scoped>
-   @import '../assets/css/minxin';
-  .top{
-      overflow: hidden;
-      li{
-        width:90px;
-        height: 28px;
-        line-height: 28px;
-        text-align: center;
-        cursor: pointer;
-        float: left;
-        @include sc(28px,#000);
-      }
-      
+@import "../assets/css/minxin";
+.top {
+  overflow: hidden;
+  .mainNavList{
+    width: 90px;
+    height: 28px;
+    line-height: 28px;
+    text-align: center;
+    cursor: pointer;
+    float: left;
+    position: relative;
+    @include sc(18px,#000);
+    .deputyNavs{
+      position: absolute;
+    }
   }
+}
 </style>
--- webskill-root/src/store/headerStore.js
+++ webskill-root/src/store/headerStore.js
@@ -2,22 +2,26 @@ import axios from 'axios'
 
 export default {
   state: {
-    //页签
-    navT:[]
+    //主导航和二级导航
+    mainNavs: []
   },
   mutations: {
-    setNav(state,navs){
-       state.navT=navs;
+    setNav(state, nav) {
+      state.mainNavs = nav;
     }
   },
   actions: {
-    getNav({commit},url){
+    getNav({
+      commit
+    }, url) {
       axios({
-        method:'get',
-        url:url
-      }).then((res)=>{
-         let data=res.data.data
-         commit('setNav',data)
+        method: 'get',
+        url: url
+      }).then((res) => {
+        let nav = res.data
+        if (nav.status == "success") {
+          commit('setNav', nav.data)
+        }
       })
     }
   }
