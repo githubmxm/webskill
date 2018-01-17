c60e31d60849461ca0be01c735beb0c3606b58aa
maoxiangmin <maoxiangmin@gyyx.cn>
2018-01-17 19:59:35 +0800

    update:个人名片

--- webskill-root/src/components/index.vue
+++ webskill-root/src/components/index.vue
@@ -11,6 +11,20 @@
         <!--广告轮播-->
         <div class="carousels left"></div>
      </div>
+     <div class="mainCon clear">
+       <div class="userInfo left">
+         <p class="headImg"><img src="../assets/images/photo.png" alt="头像" /></p>
+         <p class="hei blogger"><span>博主:</span> <span class="cor blogName">StarFire</span></p>
+         <p class="hei blogger"><span>被关注:</span> <span class="cor focus">0</span></p>
+         <p class="hei blogger"><span>被赞:</span> <span class="cor focus">0</span></p>
+         <p class="hei blogger"><span>被访问:</span> <span class="cor lookPv">0</span></p>
+         <p class="hei blogger"><span>联系方式(QQ,WX):</span> <span class="cor lookPv">1123360735</span></p>
+         <p class="PlayTour clear">
+           <span>打赏支持:</span>
+           <img src="../assets/images/wxrwm.png" alt="二维码" />
+         </p>
+       </div>
+     </div>
   </div>
 </template>
 
@@ -57,6 +71,48 @@ export default {
       width:860px;
       height:278px;
       background: red;
+    }
+  }
+  .mainCon{
+    .userInfo{
+      width: 322px;
+      padding: 10px 30px;
+      border: 1px solid #ccc;
+      .headImg{
+        width:60px;
+        height: 60px;
+        margin:10px auto;
+        img{
+          width:100%;
+          height: 100%;
+          border-radius: 50%;
+        }
+      }
+      .hei{
+        height: 30px;
+        line-height: 30px;
+        font-size: 14px;
+      }
+      .PlayTour{
+        margin: 15px auto;
+        span{
+          display: inline-block;
+          font-size: 14px;
+          margin-bottom: 8px;
+        }
+        img{
+          width:100px;
+          height:100px;
+        }
+      }
+      .blogger{
+        .cor{
+           color:#0366d6;
+        }
+        .blogName{
+          
+        }
+      }
     }
   }
 
