2df383d9ec8991c149f62237b643cce8c244dd6a
maoxiangmin <maoxiangmin@gyyx.cn>
2018-01-19 19:53:03 +0800

    update:无缝滚动

--- webskill-root/src/components/content/newDigest.vue
+++ webskill-root/src/components/content/newDigest.vue
@@ -1,30 +1,11 @@
 <template>
-  <ul class="newDigest clear">
-      <!-- <li class="digList">
-        <span class="digType left">[ js ]</span><a class="digCon ellipsis left" href="">等宽高计算</a> <span class="digetTime">20180117</span>
+<div id="box">
+    <ul class="newDigest clear"  ref="newDigest" :class="{anim:animate==true}">
+        <li class="digList" v-for="diges in newDegistList" :key="diges.id">
+          <span class="digType left">[ {{diges.newDigestType}} ]</span><a class="digCon ellipsis left" href="">{{diges.newDigestCon}}</a> <span class="digetTime">{{diges.newDigestTime}}</span>
       </li>
-      <li class="digList">
-        <span class="digType left">[ js ]</span><a class="digCon ellipsis left" href="">等宽高计算</a> <span class="digetTime">20180117</span>
-      </li> -->
-      <swiper :options="swiperOption2"  ref="newDigestSwiper">
-        <swiper-slide v-for="diges in newDegistList" :key="diges.id">
-          <li class="digList">
-            <span class="digType left">[ {{diges.newDigestType}} ]</span><a class="digCon ellipsis left" href="">{{diges.newDigestCon}}</a> <span class="digetTime">{{diges.newDigestTime}}</span>
-         </li>
-        </swiper-slide>
-        <!-- <swiper-slide>
-          <li class="digList">
-            <span class="digType left">[ js ]</span><a class="digCon ellipsis left" href="">111</a> <span class="digetTime">111</span>
-         </li>
-        </swiper-slide>
-        <swiper-slide>
-          <li class="digList">
-            <span class="digType left">[ js ]</span><a class="digCon ellipsis left" href="">111</a> <span class="digetTime">111</span>
-         </li>
-        </swiper-slide> -->
-        <div class="swiper-pagination2" slot="pagination"></div>
-      </swiper>
-  </ul>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -35,20 +16,12 @@ export default {
   data() {
     return {
       msg: "最新摘要",
-      swiperOption2: {
-        direction : 'vertical',
-        // slidesPerView: 'auto',
-        centeredSlides: true,
-        paginationClickable: true,
-        autoplay:true,//自动滚动
-        loop: true,
-        notNextTick: true,
-        observer:true,//修改swiper自己或子元素时，自动初始化swiper
-        observeParents:true,//修改swiper的父元素时，自动初始化swiper
-        height:30
-      },
+      animate:false,
       newDegistList:[]
     };
+  },
+  created(){
+    setInterval(this.scroll,3000);
   },
   methods: {
     getNewDegest(){
@@ -62,13 +35,19 @@ export default {
           _this.newDegistList=digests.data;
         }
       })
+    },
+    scroll(){
+      let newDigest = this.$refs.newDigest;
+      newDigest.style.marginTop='-30px';
+      this.animate=!this.animate;
+      var that = this; 
+      setTimeout(function(){
+          that.newDegistList.push(that.newDegistList[0]);
+          that.newDegistList.shift();
+          newDigest.style.marginTop='0px';
+          that.animate=!that.animate;  
+      },500)
     }
-    // initializeSwiper:function(){
-    //   var digets=new swiper('.swiper-pagination2',{})
-    // }
-    // swiper() {
-    //   return this.$refs.newDigestSwiper.swiper;
-    // }
   },
   components: {
     swiper,
@@ -76,43 +55,45 @@ export default {
   },
   mounted() {
     this.getNewDegest()
-    // this.swiper().startAutoplay()
-    // this.swiper().reLoop()
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.newDigest {
-  width: 310px;
-  height: 234px;
-  padding: 8px 15px;
-  background: url("../../assets/images/indexBgW.png") center center;
+#box{
   overflow: hidden;
-  .digList {
-    height: 30px;
-    line-height: 30px;
-    font-size: 14px;
-    color: #fff;
-    .digType {
-      color: rgb(115, 60, 218);
-    }
-    .digCon {
-      width: 165px;
-      margin-left: 8px;
-      &:hover {
-        text-decoration: underline;
-        color: #3498db;
+  transition: all 0.5s;
+  .anim{
+    transition: all 0.5s;
+  }
+  .newDigest {
+    width: 310px;
+    height: 225px;
+    padding: 8px 15px;
+    background: url("../../assets/images/indexBgW.png") center center;
+    overflow: hidden;
+    .digList {
+      height: 30px;
+      line-height: 30px;
+      font-size: 14px;
+      color: #fff;
+      .digType {
+        color: rgb(115, 60, 218);
+      }
+      .digCon {
+        width: 165px;
+        margin-left: 8px;
+        &:hover {
+          text-decoration: underline;
+          color: #3498db;
+        }
+      }
+      .digetTime {
+        float: right;
+        color: #ababab;
       }
     }
-    .digetTime {
-      float: right;
-      color: #ababab;
-    }
   }
-}
-.swiper-slide{
-  height:auto !important;
 }
 </style>
 
