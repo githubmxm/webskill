bd8cd7745472cffe9df5b8e95ded2bb140686154
mxm <1123360735@qq.com>
2018-01-22 00:02:00 +0800

    update:详情页优化

--- webskill-root/src/components/post.vue
+++ webskill-root/src/components/post.vue
@@ -11,7 +11,19 @@
           </div>
           <!--评论内容-->
           <div class="comments">
-            <p class="conTitle">评论专区&nbsp;&nbsp;<a href="#editor-container">【点我去评论 】</a></p>
+            <div id="comment_form">
+              <div id="commentsbmitarear">
+                <div class="guest_link">
+                  <span class="log_ico">
+                    <i class="icon iconfont icon-yonghu"></i>
+                  </span>
+                  <span class="txt">
+                    目前您尚未登录，请 <a href="/login">登录</a> 后进行评论
+                   <!-- 评论专区&nbsp;&nbsp;<a>【点我去评论 】</a> -->
+                  </span>
+                  </div>
+                </div>
+            </div>
             <ul class="commentList">
               <li class="commentD">
                 <p class="commentInfo clear">
@@ -25,6 +37,7 @@
                   <p class="commentHandleType right">
                     <span class="comLike">1</span>
                     <span class="comTread">1</span>
+                    <span class="replay">回复</span>
                   </p>
                 </div>
               </li>
@@ -102,6 +115,7 @@ export default {
         .detCon{
           font-size: 16px;
           padding:20px 20px 20px 0;
+          border-bottom:1px solid #ccc;
         }
         .comments{
           padding:20px 20px 20px 0;
@@ -112,6 +126,49 @@ export default {
             text-align:center;
             font-size:14px;
           }
+          #comment_form{
+            #commentsbmitarear{
+              .guest_link{
+                margin-bottom: 20px;
+                clear: both;
+                overflow: hidden;
+                height: 80px;
+                font-family: MicrosoftYaHei;
+                font-size: 14px;
+                color: #4f4f4f;
+                text-align: center;
+                background: #e7ecf0;
+                .log_ico{
+                  width: 40px;
+                  height: 40px;
+                  border-radius: 20px;
+                  line-height: 40px;
+                  margin-top: 20px;
+                  margin-right: 20px;
+                  text-align: center;
+                  background: #afbac3;
+                  display: inline-block;
+                  vertical-align: top;
+                  i{
+                    color: #e2e9ef;
+                    font-family: iconfont!important;
+                    -webkit-font-smoothing: antialiased;
+                    -moz-osx-font-smoothing: grayscale;
+                    font-style: normal;
+                    font-size: 16px;
+                  }
+                }
+                .txt{
+                  display: inline-block;
+                  margin-top: 28px;
+                  a{
+                    color: #e73131;
+                    text-decoration: underline;
+                  }
+                }
+              }
+            }
+          }
           .commentList{
             .commentD{
               padding: 10px 0;
@@ -121,6 +178,13 @@ export default {
                 font-size:14px;
                 border-bottom: 1px dashed #ccc;
                 padding-bottom: 8px;
+                .commentUser{
+                  color: #4093c6;
+                }
+                .commentTime,.time{
+                  color: #ccc;
+                }
+
               }
               .comcon{
                 margin-top: 12px;
@@ -129,7 +193,7 @@ export default {
                 margin:8px 0;
                 .commentHandleType{
                   display:inline-block;
-                  .comTread,.comLike{
+                  .comTread,.comLike,.replay{
                     display:inline-block;
                     padding-left: 22px;
                     width: 20px;
@@ -144,6 +208,10 @@ export default {
                   }
                   .comTread{
                     background-position: 0 -120px; 
+                  }
+                  .replay{
+                    width: 47px;
+                    background-position: left -200px; 
                   }
                 }
               }
