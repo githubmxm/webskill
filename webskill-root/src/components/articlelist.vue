9d7ae00bbea1b4f3afb7eb48b1cac11845c2a61c
mxm <1123360735@qq.com>
2018-02-05 22:37:16 +0800

    update:模块专题优化

--- webskill-root/src/components/articlelist.vue
+++ webskill-root/src/components/articlelist.vue
@@ -5,17 +5,30 @@
     <div class="articleList clear">
         <div class="left col-lg-9 col-sm-8 col-xs-12">
             <div class="conLf col-lg-11 col-sm-11 col-xs-12">
-                <h1 class="skillType">{{articelSkillType}}</h1>
-                <section class="clear skillListCon">
-                    <p class="noList" v-if="!skillListDetail.length">
+                <p class="noList" v-if="!skillListDetail.length">
                         内容更新中...敬请期待！！！
-                    </p>
-                    <div v-else>
-                        <ul class="skillDigetList">
-                            <li>
-                                111
-                            </li>
-                        </ul>
+                </p>
+                <section class="clear skillListCon"  v-else>
+                    <div class="skillDigetList">
+                        <article class="articles">
+                           <div class="arBoder">
+                                <header class="articleHeader">
+                                    <h1><a class="headerTitle" href="">文章标题</a></h1>
+                                </header>
+                                <div class="articleCons">
+                                    <p class="articleDigest">摘要</p>
+                                </div>
+                                <p class="articleReadAll">
+                                    <a href="">阅读全文</a>
+                                </p>
+                           </div>
+                           <footer class="articleFooter clear">
+                                <span class="arTit left">js#win使用</span>
+                                <p class="arItem right">
+                                    <span class="time">2018-01-10</span>&nbsp;&nbsp;<span class="comment"><i class="itRed">0</i>次评论</span>&nbsp;&nbsp;<span class="readed"><i class="itRed">0</i>次阅览</span>
+                                </p>
+                            </footer>
+                        </article> 
                     </div>
                 </section>
             </div>
@@ -46,6 +59,9 @@ export default{
     .conLf,.adverLf{
         background: #fff;
         padding: .2rem;
+        .noList{
+            font-size:.18rem;
+        }
     }
     .skillType{
         font-size:.22rem;
@@ -58,10 +74,67 @@ export default{
             text-align: center;
         }
         .skillDigetList{
-            margin:18px 0;
-            li{
-                font-size:.18rem;
+            font-size:.18rem;
+            padding:.1rem;
+            padding-bottom: 25px;
+            .articles{
+                .arBoder{
+                    border-bottom: 1px solid #ccc;
+                    padding-bottom: .2rem;
+                    .articleHeader{
+                        .headerTitle{
+                            display: inline-block;
+                            font-size:.22rem;
+                            margin:10px 0;
+                        }
+                    }
+                    .articleCons{
+                        margin: .1rem 0 .2rem 0;
+                        font-size:.16rem;
+                        .articleDigest{
+
+                        }
+                    }
+                    .articleReadAll{
+                        display: inline-block;
+                        line-height: 1em;
+                        padding: .06rem .15rem;
+                        border-radius: .15rem;
+                        background: #eee;
+                        color: #999;
+                        text-shadow: 0 1px #fff;
+                        text-decoration: none;
+                        a{
+                            font-size:.16rem;
+                            color:#999;
+                        }
+                        &:hover{
+                            background: red;
+                           a{
+                            text-decoration: none;
+                            color: #fff;
+                           }
+                        }
+                    }
+                }
+                .articleFooter{
+                    font-size:.14rem;
+                   margin-top:.2rem;
+                   .arTit{
+                       color: #ccc;
+                   }
+                   .arItem{
+                       .time,.comment,.readed{
+                           color:#ccc;
+                       }
+                       .itRed{
+                           color: red;
+                       }
+                   }
+                }
+                
             }
+            
         }
     }
     
