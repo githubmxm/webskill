62c74006c874143890bca1f6fbc469a4fa404d7b
mxm <1123360735@qq.com>
2018-03-17 22:14:33 +0800

    update:留言管理列表

--- webskill-root/src/components/surePublish.vue
+++ webskill-root/src/components/surePublish.vue
@@ -4,10 +4,11 @@
 <template>
     <div class="surePublish row">
         <p class="navbar-header col-xs-12">
-          <span class="navbar-brand col-xs-6 pull-left" :class="{cur:myPostList==0}"  @click="postPublishType(0)">待发布{{error}}</span>
-          <span class="navbar-brand col-xs-6 pull-left" :class="{cur:myPostList==1}" @click="postPublishType(1)">已发布{{errorHas}}</span>
+          <span class="navbar-brand col-xs-4 pull-left" :class="{cur:myPostList==0}"  @click="postPublishType(0)">待发布{{error}}</span>
+          <span class="navbar-brand col-xs-4 pull-left" :class="{cur:myPostList==1}" @click="postPublishType(1)">已发布{{errorHas}}</span>
+          <span class="navbar-brand col-xs-4 pull-left" :class="{cur:myPostList==2}" @click="postPublishType(2)">留言区{{leaveErr}}</span>
         </p>
-        <ul class="post-list list-group col-xs-12">
+        <ul class="post-list list-group col-xs-12" v-if="myPostList<2">
             <li class="list-group-item col-xs-12" v-for="item in publishListDetail">
                 <span class="title col-xs-8 col-md-9 col-lg-11">{{item.publishPostTitle}}【{{item.publishPostAuthor}}】</span>
                 <span class="look col-xs-4 col-md-3 col-lg-1 text-center">
@@ -16,6 +17,14 @@
                 </span>
             </li>
         </ul>
+        <ul class="post-list list-group col-xs-12" v-if="myPostList==2">
+                <li class="list-group-item col-xs-12" v-for="item in publishListDetail">
+                    <span class="title col-xs-8 col-md-9 col-lg-11">{{item.leaveword_conts}}【{{item.leaveAuthor}}】-【{{item.leaveType | chainLeaveType}}】</span>
+                    <!-- <span class="look col-xs-4 col-md-3 col-lg-1 text-center">
+                        <router-link v-if="myPostList==0" :to="'/previewpost/'+item.publishPostId" target="_blank" class="headerTitle">查看详情</router-link>
+                    </span> -->
+                </li>
+            </ul>
         <Pagebar v-show="publishListDetail.length>0" :page-model="pageModel" ref="publishPostListPage"></Pagebar>
     </div>    
 </template>
@@ -32,12 +41,27 @@ export default {
             error:"",
             errorHas:"",
             myPostList:0,
+            leaveErr:"",
             pageModel:{
                 url:"/webskill/getSurePublish",
                 myPostList:0,
                 againPost:0
             }
         };
+    },
+    filters: {
+        chainLeaveType(leaveType){
+            switch(leaveType){
+                case 0: return '私密';
+                break;
+                case 1: return 'Bug';
+                break;
+                case 2: return '建议';
+                break;
+                case 3: return '技能解答';
+                break;
+            }
+        }
     },
     methods: {
         postPublishType(index){
